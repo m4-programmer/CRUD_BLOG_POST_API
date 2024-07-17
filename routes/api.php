2c0cf0b94997def24a27699f6dd7a07d7c0c1139
Adolphus Miracle <miraboy13@gmail.com>
2024-07-17 14:25:31 +0100

    feat: implemented api's

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+
+Route::prefix('v1')->group(function (){
+    Route::apiResource('blog', BlogController::class);
+    Route::post('post/{post}/like',[PostController::class,'likePost']);
+    Route::post('post/{post}/comment',[PostController::class,'comment']);
+    Route::apiResource('post', PostController::class);
+});
