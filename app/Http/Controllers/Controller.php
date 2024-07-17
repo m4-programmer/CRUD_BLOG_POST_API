f9cd8558aa6d29049a2f7536e485a503deb56bc1
Adolphus Miracle <miraboy13@gmail.com>
2024-07-17 14:33:34 +0100

    feat: created helper response methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,21 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function success($data = [], $message = "Successful", $status=200)
+    {
+        return response()->json([
+            "message" => $message ?? "Successful",
+            "status" => $status,
+            "data"=>$data,
+        ], $status);
+    }
+
+    public function error($message = "An error Occurred", $status=500, $error = [])
+    {
+        return response()->json([
+            "message" => $message ,
+            "status" => $status,
+            "error"=>$error,
+        ], $status);
+    }
 }
